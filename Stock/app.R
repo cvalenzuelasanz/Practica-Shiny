e21300bae72dc4a048987d85f3ade21eab1931bb
cvalenzuelasanz <32160463+cvalenzuelasanz@users.noreply.github.com>
2017-12-18 15:25:27 +0100

    Segunda version

--- Stock/app.R
+++ Stock/app.R
@@ -8,36 +8,74 @@ library(quantmod)
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
-    dashboardHeader(),
-    dashboardSidebar(
-      selectInput(inputId = "stocks",
-                  label = "Select stock",
-                  choices = list("Google" = "GOOG","Apple"= "AAPL","Microsoft"= "MSFT","Banco Santander" = "SAN.MC",
-                                 "BBVA" = "BBVA.MC"),
-                  selected = 1
+  dashboardHeader(),
+  dashboardSidebar(
+    selectInput(inputId = "stocks",
+                label = "Select stock",
+                choices = list("Google" = "GOOG","Apple"= "AAPL","Microsoft"= "MSFT","Banco Santander" = "SAN.MC",
+                               "BBVA" = "BBVA.MC"),
+                selected = 1
     ),
     dateRangeInput("dates", 
                    "Date range",
                    start = "2013-01-01", 
-                   end = as.character(Sys.Date()))
-    
-),
-    dashboardBody(
-      box(plotOutput("plot"))
+                   end = as.character(Sys.Date())),
+    sidebarMenu(
+      menuItem("Ramdom",tabName = "Random_strategy",icon=icon("line_chart")),
+      menuItem("Limits",tabName = "Limits_strategy",icon=icon("balance-scale")),
+      menuItem("Programming",tabName = "Programming_strategy",icon=icon("edit"))
     )
+    
+  ),
+  dashboardBody(
+    box(plotOutput("plot")),
+    tabItems(
+      tabItem(tabName = "Random_strategy",
+              fluidPage(
+                mainPanel(
+                  verbatimTextOutput("muestra"),
+                 
+                  tags$div(style = "position: absolute; right: 0; botton:0",
+                            tags$input(type= "text",ID= "cantidad_comprada",
+                             tags$label(class = "right","buy"),
+                             value = 1))
+                  # tags$div( style="position: absolute; left: %; botton: 100%",
+                  #                     numericInput("numero_dias_compra",label="% of the days",value=0))
+                  # tags$div(style = "position: absolute; right: 100%; top: 100",
+                  #          dateInput("start_date","start date",value = "2017-01-01")),
+                  # tags$div(style = "position: absolute; left: 100%; top: 100",
+                  #   dateInput("end_date","end date",value = "2017-12-01")),
+                  # actionButton("Run_random","Run")
+                  ))),
+      tabItem(tabName = "Limits_strategy",
+              fluidPage(
+                sidebarLayout(
+                  sidebarPanel(),
+                  mainPanel()
+                  ))),
+      tabItem(tabName = "Programming_strategy",
+              fluidPage(
+                sidebarLayout(
+                  sidebarPanel(),
+                  mainPanel()
+                )))
+    )
+  )
 )
+
 
 
 server <- function(input, output) {
   
   
-    dataInput <- reactive({
-      getSymbols(input$stocks, src = "yahoo", 
-                 from = input$dates[1],
-                 to = input$dates[2],
-                 auto.assign = FALSE)
-    })
-
+  
+  dataInput <- reactive({
+    getSymbols(input$stocks, src = "yahoo", 
+               from = input$dates[1],
+               to = input$dates[2],
+               auto.assign = FALSE)
+  })
+  
   
   output$plot <- renderPlot({
     
